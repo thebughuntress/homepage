92c2e442ee9d60db10f3b536d7738e600acba712
Frey, Antonia (EXTERN: Devoteam) <antonia.frey@devoteam.com>
2025-01-07 22:05:25 +0100

    Styling hero

--- src/components/Hero2/Hero2.jsx
+++ src/components/Hero2/Hero2.jsx
@@ -106,7 +106,7 @@ function Hero2() {
               whiteSpace: "nowrap",
               overflow: "hidden",
               marginY: 1,
-              height: "65px",
+              height: {xs: "48px", md: "64px"},
             }}
           >
             {role}
